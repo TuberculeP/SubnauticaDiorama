f2cf32fceabab7c70c6675ebd59ba08c99122015
Félix Laviéville <lavieville.felix@gmail.com>
2025-11-09 21:15:21 +0100

    fix: testing correction

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    fs: {
+      strict: true
+    }
+  },
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  assetsInclude: ['**/*.glb', '**/*.gltf']
 })
